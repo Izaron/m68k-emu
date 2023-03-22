72636d2211936d6d4ef71cf56b351ea0c8da17c0
Evgeny Shulgin <izaronplatz@gmail.com>
2023-03-22 21:10:07 +0000

    Better tests

--- src/lib/emulator/test/main.cpp
+++ src/lib/emulator/test/main.cpp
@@ -1,5 +1,7 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <set>
 
 #include <lib/emulator/emulator.h>
 
@@ -9,9 +11,14 @@ using json = nlohmann::json;
 
 namespace {
 
+using TRamSnapshot = std::vector<std::pair<NMemory::TAddressType, std::byte>>;
+
 class TTestDevice : public NMemory::IDevice {
 public:
     TTestDevice(int pc, const json& prefetch, const json& ram) {
+        // remember the PC
+        PC_ = pc;
+
         // fill RAM
         for (const auto& pair : ram) {
             const auto addr = pair[0].get<int>();
@@ -23,13 +30,23 @@ public:
         if (prefetch.size() != 2) {
             throw std::runtime_error("why not 2 items?");
         }
-        Values_[pc] = static_cast<std::byte>(prefetch[0].get<int>() >> 8);
-        Values_[pc + 1] = static_cast<std::byte>(prefetch[0].get<int>() % 256);
-        Values_[pc + 2] = static_cast<std::byte>(prefetch[1].get<int>() >> 8);
-        Values_[pc + 3] = static_cast<std::byte>(prefetch[1].get<int>() % 256);
+        Prefetch_.push_back(static_cast<std::byte>(prefetch[0].get<int>() >> 8));
+        Prefetch_.push_back(static_cast<std::byte>(prefetch[0].get<int>() % 256));
+        Prefetch_.push_back(static_cast<std::byte>(prefetch[1].get<int>() >> 8));
+        Prefetch_.push_back(static_cast<std::byte>(prefetch[1].get<int>() % 256));
     }
 
     NMemory::TDataHolder Read(NMemory::TAddressType addr, NMemory::TAddressType size) override {
+        if (addr == PC_) {
+            if (size != 2) {
+                throw std::runtime_error("why not 2 bytes?");
+            }
+            NMemory::TDataHolder res;
+            res.push_back(Prefetch_[0]);
+            res.push_back(Prefetch_[1]);
+            return res;
+        }
+
         NMemory::TDataHolder res;
         for (int i = addr; i < addr + size; ++i) {
             if (const auto it = Values_.find(addr); it != Values_.end()) {
@@ -43,13 +60,49 @@ public:
 
     void Write(NMemory::TAddressType addr, NMemory::TDataView data) override {
         for (const auto value : data) {
-            Values_[addr++] = value;
+            if (value != static_cast<std::byte>(0)) {
+                Values_[addr] = value;
+            }
+            addr++;
         }
     }
 
+    TRamSnapshot MakeRamSnapshot() const {
+        TRamSnapshot res;
+        for (const auto& p : Values_) {
+            res.emplace_back(p.first, p.second);
+        }
+        return res;
+    }
+
 private:
-    std::map<int, std::byte> Values_;
+    int PC_;
+    std::vector<std::byte> Prefetch_;
+    std::map<NMemory::TAddressType, std::byte> Values_;
 };
+
+std::string DumpRamSnapshot(const TRamSnapshot& ram) {
+    std::stringstream ss;
+    for (const auto& pair : ram) {
+        ss << "[" << pair.first << "] = " << static_cast<int>(pair.second) << "\n";
+    }
+    return ss.str();
+}
+
+TRamSnapshot MakeRamSnapshot(const json& ram) {
+    std::map<NMemory::TAddressType, std::byte> values;
+    for (const auto& pair : ram) {
+        const auto addr = pair[0].get<int>();
+        const auto value = pair[1].get<std::byte>();
+        values[addr] = value;
+    }
+
+    TRamSnapshot res;
+    for (const auto& p : values) {
+        res.emplace_back(p.first, p.second);
+    }
+    return res;
+}
 
 json LoadTestFile(std::string_view path) {
     std::ifstream f(path.data());
@@ -58,18 +111,29 @@ json LoadTestFile(std::string_view path) {
     return data;
 }
 
-bool Equals(const NRegisters::TRegisters& lhs, const NRegisters::TRegisters& rhs) {
+std::optional<std::string> DumpDiff(const NRegisters::TRegisters& lhs, const NRegisters::TRegisters& rhs) {
+    std::vector<std::string> diffs;
+
     for (int i = 0; i < 8; ++i) {
-        if (lhs.D[i] != rhs.D[i]) return false;
+        if (lhs.D[i] != rhs.D[i]) diffs.emplace_back("D" + std::to_string(i));
     }
     for (int i = 0; i < 7; ++i) {
-        if (lhs.A[i] != rhs.A[i]) return false;
+        if (lhs.A[i] != rhs.A[i]) diffs.emplace_back("A" + std::to_string(i));
     }
-    if (lhs.USP != rhs.USP) return false;
-    if (lhs.SSP != rhs.SSP) return false;
-    if (lhs.PC != rhs.PC) return false;
-    if (lhs.SR != rhs.SR) return false;
-    return true;
+    if (lhs.USP != rhs.USP) diffs.emplace_back("USP");
+    if (lhs.SSP != rhs.SSP) diffs.emplace_back("SSP");
+    if (lhs.PC != rhs.PC) diffs.emplace_back("PC");
+    if (lhs.SR != rhs.SR) diffs.emplace_back("SR");
+
+    if (diffs.empty()) {
+        return std::nullopt;
+    } else {
+        std::string info = "";
+        for (const auto& d : diffs) {
+            info += d + " ";
+        }
+        return info;
+    }
 }
 
 NRegisters::TRegisters ParseRegisters(const json& j) {
@@ -100,15 +164,40 @@ bool WorkOnTest(const json& test) {
     NEmulator::Emulate(ctx);
 
     // TODO: compare RAM
-    if (!Equals(expectedFinal, actualFinal)) {
-        std::cerr << "Initial registers:" << std::endl;
-        std::cerr << Dump(initial) << std::endl;
+    const auto regsDiff = DumpDiff(expectedFinal, actualFinal);
+    const auto actualRam = MakeRamSnapshot(device.MakeRamSnapshot());
+    const auto expectedRam = MakeRamSnapshot(finalJson["ram"]);
 
-        std::cerr << "Actual final registers:" << std::endl;
-        std::cerr << Dump(actualFinal) << std::endl;
+    if (regsDiff || expectedRam != actualRam) {
+        std::cerr << "Test name: \"" << test["name"].get<std::string>() << "\"" << std::endl << std::endl;
 
-        std::cerr << "Expected final registers:" << std::endl;
-        std::cerr << Dump(expectedFinal) << std::endl;
+        if (regsDiff) {
+            std::cerr << "Initial registers:" << std::endl;
+            std::cerr << Dump(initial) << std::endl;
+
+            std::cerr << "Actual final registers:" << std::endl;
+            std::cerr << Dump(actualFinal) << std::endl;
+
+            std::cerr << "Expected final registers:" << std::endl;
+            std::cerr << Dump(expectedFinal) << std::endl;
+
+            std::cerr << "Differing registers: " << *regsDiff << std::endl << std::endl;
+        }
+
+        if (expectedRam != actualRam) {
+            std::cerr << "Initial RAM:" << std::endl;
+            std::cerr << DumpRamSnapshot(MakeRamSnapshot(initialJson["ram"])) << std::endl;
+
+            std::cerr << "Actual RAM:" << std::endl;
+            std::cerr << DumpRamSnapshot(actualRam) << std::endl;
+
+            std::cerr << "Expected RAM:" << std::endl;
+            std::cerr << DumpRamSnapshot(expectedRam) << std::endl;
+
+            std::cerr << "RAM differs" << std::endl;
+        }
+
+        //std::cerr << "Test source: \"" << test.dump(4) << std::endl;
 
         return false;
     }
@@ -133,6 +222,21 @@ bool WorkOnFile(const json& file) {
 } // namespace
 
 int main() {
-    auto file = LoadTestFile("/home/mango/proctest/NOP.json");
-    WorkOnFile(file);
+    namespace fs = std::filesystem;
+
+    std::set<std::string> paths;
+    for (const auto& entry : fs::directory_iterator("/home/mango/ProcessorTests/680x0/68000/v1")) {
+        auto path = entry.path().string();
+        if (!path.ends_with(".json")) {
+            continue;
+        }
+        paths.emplace(std::move(path));
+    }
+
+    for (const auto& path : paths) {
+        auto file = LoadTestFile(path);
+        if (!WorkOnFile(file)) {
+            break;
+        }
+    }
 }
--- src/lib/registers/registers.cpp
+++ src/lib/registers/registers.cpp
@@ -6,7 +6,7 @@ namespace NRegisters {
 
 std::string Dump(const TRegisters& r) {
     std::stringstream ss;
-    ss << std::hex << std::uppercase;
+    //ss << std::hex << std::uppercase;
     for (int i = 0; i < 7; ++i) {
         ss << "D" << i << " = " << r.D[i] << "\tA" << i << " = " << r.A[i] << "\n";
     }
@@ -14,7 +14,18 @@ std::string Dump(const TRegisters& r) {
     ss << "USP = " << r.USP << "\n";
     ss << "SSP = " << r.SSP << "\n";
     ss << "PC = " << r.PC << "\n";
-    ss << "SR = " << r.SR << "\n";
+
+    ss << "SR: ";
+    ss << "T = " << ((r.SR >> 14) & 3) << ", ";
+    ss << "S = " << ((r.SR >> 13) & 1) << ", ";
+    ss << "M = " << ((r.SR >> 12) & 1) << ", ";
+    ss << "I = " << ((r.SR >> 8) & 7) << ", ";
+    ss << "X = " << ((r.SR >> 4) & 1) << ", ";
+    ss << "N = " << ((r.SR >> 3) & 1) << ", ";
+    ss << "Z = " << ((r.SR >> 2) & 1) << ", ";
+    ss << "V = " << ((r.SR >> 1) & 1) << ", ";
+    ss << "C = " << (r.SR & 1) << "\n";
+
     return ss.str();
 }
 
